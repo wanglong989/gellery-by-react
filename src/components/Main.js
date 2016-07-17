d57704b2c72da9f65dda6e0fb3eb5596d081fbe1
wanglong <wanglong989@sina.com>
2016-07-17 11:17:39 +0800

    图片墙-->react

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,19 +4,45 @@ require('styles/App.css');
 import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
+/**
+ * 获取图片相关的数据 利图片名转换成 URL 路径信息
+ *
+ */
+
+let picData = require('../data/pic.json');
+let imageDatas = (function genImagesURL(imagePicDataArr){
+  for(var i = 0;i < imagePicDataArr.length;i++){
+    var singleImageData = imagePicDataArr[i];
+
+    singleImageData.imageURL = require('../images/'+singleImageData.fileName);
+
+    imagePicDataArr[i] = singleImageData;
+  }
+  return imagePicDataArr;
+})(picData);
+
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit  <code>src/components/Main.js</code> to get started!</div>
+
+        <section className="stage">
+          <section className="img-sec">
+            ww
+          </section>
+          <nav className="controller-nav">
+            ww
+          </nav>
+        </section>
+
       </div>
     );
   }
 }
 
 AppComponent.defaultProps = {
+
 };
 
 export default AppComponent;
