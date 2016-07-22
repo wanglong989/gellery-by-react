09e63be1535d861dd2f66587cfc914e375c8a097
wanglong <wanglong989@sina.com>
2016-07-22 14:25:57 +0800

    图片墙-->react

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -37,7 +37,12 @@ function getDefaultModules() {
       {
         test: /\.(mp4|ogg|svg)$/,
         loader: 'file-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
       }
+
     ]
   };
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//let yeomanImage = require('../images/yeoman.png');
 /**
  * 获取图片相关的数据 利图片名转换成 URL 路径信息
  *
@@ -11,34 +11,68 @@ let yeomanImage = require('../images/yeoman.png');
 
 let picData = require('../data/pic.json');
 let imageDatas = (function genImagesURL(imagePicDataArr){
+
   for(var i = 0;i < imagePicDataArr.length;i++){
+
     var singleImageData = imagePicDataArr[i];
 
     singleImageData.imageURL = require('../images/'+singleImageData.fileName);
-
+    singleImageData.key = i+1;
     imagePicDataArr[i] = singleImageData;
+
   }
   return imagePicDataArr;
+
 })(picData);
 
 
-class AppComponent extends React.Component {
-  render() {
+var ImgFigure = React.createClass({
+  render: function () {
     return (
+        <figure className="img-figure">
+
+          <img src={this.props.imageURL}  alt={this.props.title}/>
+          <figcaption>
+            <h2>{this.props.desc}</h2>
+          </figcaption>
+        </figure>
+    );
+  }
+});
+
+
+class AppComponent extends React.Component {
+
+  render() {
+
+    var controllerUnits = [],
+        imgFigure = [],
+        activeIndex = 0;
+    imageDatas.forEach(function(value,i){
+      var activeFlag = '';
+      if(activeIndex == 0){
+        activeFlag = 'active';
+      }
+      imgFigure.push(<ImgFigure key={value.key} title={value.title} desc={value.desc} imageURL={value.imageURL} />);
+    });
+
+    return (
+
       <div className="index">
 
         <section className="stage">
           <section className="img-sec">
-            ww
+            {imgFigure}
           </section>
           <nav className="controller-nav">
-            ww
+            {controllerUnits}
           </nav>
         </section>
 
       </div>
     );
   }
+
 }
 
 AppComponent.defaultProps = {
